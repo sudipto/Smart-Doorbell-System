3838713f1af13e931c6e2d6f90df55abc5f1dedb
Sudipto Mondal <sudipto.mondal1997@gmail.com>
2024-04-09 23:37:05 +0530

    made some changes and added requirements.txt

--- face_shot.py
+++ face_shot.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 name = 'elon' #replace with your name
 
@@ -10,6 +11,11 @@ cv2.resizeWindow("press space to take a photo", 500, 300)
 img_counter = 0
 
 while True:
+    # This line is using the `read()` method of a `cam` object, which is likely an instance of a VideoCapture object from the OpenCV library. The `read()` method is used to grab, decode and return the next video frame from the video file or camera.
+    # The `read()` method returns two values:
+    # 1. `ret` (short for return value): This is a boolean value. It will be True if the frame was read correctly and False if not (for example, if the video file is over or the camera is not open).
+    # 2. `frame`: This is an image array vector captured by the webcam (frame). It represents the next video frame. If the frame is read correctly, it will be a numpy array.
+    # The line of code is using Python's multiple assignment feature to assign these two return values to the variables `ret` and `frame` respectively. This is a common pattern when working with OpenCV to capture video frames.
     ret, frame = cam.read()
     if not ret:
         print("failed to grab frame")
@@ -23,7 +29,7 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        img_name = os.path.join("dataset", name, "image_{}.jpg".format(img_counter))
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
